992bf4d9d9e9e0efdd421c916a6b3789a583f21c
Ole Holgernes <ole.holgernes@gmail.com>
2022-08-15 14:47:59 +0200

    add wip extension script

---
+++ svelte_extension.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#****************************************************
+#* Extends basic Flask app with Svelte, sets up     *
+#* Svelte dev env.                                  *
+#* Written by Ole Holgernes.                        *
+#****************************************************
+
+#-> install node.js and npm
+apt update
+apt install nodejs npm
